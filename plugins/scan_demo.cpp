6a5c830155ca1357a9224d6d5b72450a6df43864
Simson Garfinkel <simsong@acm.org>
2012-10-10 00:12:25 -0400

    created plugins directory; it works with BE1.3

--- plugins/scan_demo.cpp
+++ plugins/scan_demo.cpp
@@ -8,10 +8,11 @@
  * It also generates a feature report for every likely JPEG.
  */
 
+#include "beconfig.h"
+#include "bulk_extractor_i.h"
+
 #include <iostream>
 #include <sys/types.h>
-
-#include "../src/bulk_extractor_i.h"
 
 static uint64_t count_null512=0;
 static uint64_t count_jpeg=0;
@@ -32,36 +33,39 @@ static bool is_zero(const u_char *buf,size_t count)
 
 
 extern "C"
-void  scan_bulk(const class scanner_params &sp,const recursion_control_block &rcb)
+void  scan_demo(const class scanner_params &sp,const recursion_control_block &rcb)
 {
-    if(sp.version!=1){
-	cerr << "scan_bulk requires sp version 1; got version " << sp.version << "\n";
+    if(sp.sp_version!=scanner_params::CURRENT_SP_VERSION){
+	cerr << "scan_demo requires sp version 1; got version " << sp.sp_version << "\n";
 	exit(1);
     }
 
     /* Check for phase 0 --- startup */
-    if(sp.phase==0){
-	sp.info->name  = "bulk";
+    if(sp.phase==scanner_params::startup){
+	sp.info->name     = "demo";
+	sp.info->author  = "Simson L. Garfinkel";
 	sp.info->flags = 0;
         return; /* No feature files created */
     }
 
     /* Check for phase 2 --- shutdown */
-    if(sp.phase==2){
-	feature_recorder *alert = sp.fs->get_alert_recorder();
+    if(sp.phase==scanner_params::shutdown){
+	feature_recorder *alert = sp.fs.get_alert_recorder();
 	alert->printf("total sectors: %"PRId64,count_total);
 	alert->printf("total jpegs: %"PRId64,count_jpeg);
 	alert->printf("total nulls: %"PRId64,count_null512);
 	return;
     }
     
-    for(const u_char *b0 = sp.sbuf.buf ;
-	b0<sp.sbuf.buf+sp.sbuf.pagesize-blocksize; b0+=blocksize){
-	
-	__sync_fetch_and_add(&count_total,1);
-	if(is_zero(b0,512)) __sync_fetch_and_add(&count_null512,1);
-	if(b0[0]==0377 && b0[1]==0330 && b0[2]==0377 && b0[3]==0341){
-	    __sync_fetch_and_add(&count_jpeg,1);
+    if(sp.phase==scanner_params::scan){
+	for(const u_char *b0 = sp.sbuf.buf ;
+	    b0<sp.sbuf.buf+sp.sbuf.pagesize-blocksize; b0+=blocksize){
+	    
+	    __sync_fetch_and_add(&count_total,1);
+	    if(is_zero(b0,512)) __sync_fetch_and_add(&count_null512,1);
+	    if(b0[0]==0377 && b0[1]==0330 && b0[2]==0377 && b0[3]==0341){
+		__sync_fetch_and_add(&count_jpeg,1);
+	    }
 	}
     }
 }
