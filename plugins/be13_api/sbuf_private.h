a422459121ec101955cd8b12127cafae569ae61c
Simson Garfinkel <simsong@acm.org>
2012-10-27 18:37:20 -0400

    typos fixed; code cleaned up

--- plugins/be13_api/sbuf_private.h
+++ plugins/be13_api/sbuf_private.h
@@ -1,6 +1,8 @@
 #ifndef SBUF_PRIVATE_H
 #define SBUF_PRIVATE_H
 
+#include <unistd.h>
+
 #ifdef HAVE_MMAP
 #include <sys/mman.h>
 #endif
@@ -87,7 +89,7 @@ inline uint8_t sbuf_t::get8u(size_t i,sbuf_t::byte_order_t bo) const {
 }
 
 inline uint16_t sbuf_t::get16u(size_t i,sbuf_t::byte_order_t bo) const {
-    return bo==BO_LITTLE_ENDIAN ? get16u(i) : get8uBE(i);
+    return bo==BO_LITTLE_ENDIAN ? get16u(i) : get16uBE(i);
 }
 
 inline uint32_t sbuf_t::get32u(size_t i,sbuf_t::byte_order_t bo) const {
@@ -99,82 +101,24 @@ inline uint64_t sbuf_t::get64u(size_t i,sbuf_t::byte_order_t bo) const {
 }
 
 /**
- * Signed get interfaces
+ * Signed get interfaces simply call the unsigned interfaces and
+ * the return gets cast.
  */
-inline int8_t sbuf_t::get8i(size_t i) const {
-    if(i+1>bufsize) throw sbuf_t::range_exception_t();
-    return this->buf[i];
-}
-
-inline int16_t sbuf_t::get16i(size_t i) const {
-    if(i+2>bufsize) throw sbuf_t::range_exception_t();
-    return 0 
-	| (uint16_t)(this->buf[i+0]<<0) 
-	| (uint16_t)(this->buf[i+1]<<8);
-}
-
-inline int32_t sbuf_t::get32i(size_t i) const {
-    if(i+4>bufsize) throw sbuf_t::range_exception_t();
-    return 0 
-	| (uint32_t)(this->buf[i+0]<<0) 
-	| (uint32_t)(this->buf[i+1]<<8) 
-	| (uint32_t)(this->buf[i+2]<<16) 
-	| (uint32_t)(this->buf[i+3]<<24);
-}
-
-inline int64_t sbuf_t::get64i(size_t i) const {
-    if(i+8>bufsize) throw sbuf_t::range_exception_t();
-    return 0 
-	| ((uint64_t)(this->buf[i+0])<<0) 
-	| ((uint64_t)(this->buf[i+1])<<8) 
-	| ((uint64_t)(this->buf[i+2])<<16) 
-	| ((uint64_t)(this->buf[i+3])<<24) 
-	| ((uint64_t)(this->buf[i+4])<<32) 
-	| ((uint64_t)(this->buf[i+5])<<40) 
-	| ((uint64_t)(this->buf[i+6])<<48) 
-	| ((uint64_t)(this->buf[i+7])<<56);
-}
-
-inline int8_t sbuf_t::get8iBE(size_t i) const {
-    if(i+1>bufsize) throw sbuf_t::range_exception_t();
-    return this->buf[i];
-}
-
-inline int16_t sbuf_t::get16iBE(size_t i) const {
-    if(i+2>bufsize) throw sbuf_t::range_exception_t();
-    return 0 
-	| (uint16_t)(this->buf[i+1]<<0) 
-	| (uint16_t)(this->buf[i+0]<<8);
-}
-
-inline int32_t sbuf_t::get32iBE(size_t i) const {
-    if(i+4>bufsize) throw sbuf_t::range_exception_t();
-    return 0 
-	| (uint32_t)(this->buf[i+3]<<0) 
-	| (uint32_t)(this->buf[i+2]<<8) 
-	| (uint32_t)(this->buf[i+1]<<16) 
-	| (uint32_t)(this->buf[i+0]<<24);
-}
-
-inline int64_t sbuf_t::get64iBE(size_t i) const {
-    if(i+8>bufsize) throw sbuf_t::range_exception_t();
-    return 0 
-	| ((uint64_t)(this->buf[i+7])<<0) 
-	| ((uint64_t)(this->buf[i+6])<<8) 
-	| ((uint64_t)(this->buf[i+5])<<16) 
-	| ((uint64_t)(this->buf[i+4])<<24) 
-	| ((uint64_t)(this->buf[i+3])<<32) 
-	| ((uint64_t)(this->buf[i+2])<<40) 
-	| ((uint64_t)(this->buf[i+1])<<48) 
-	| ((uint64_t)(this->buf[i+0])<<56);
-}
+inline int8_t sbuf_t::get8i(size_t i)   const { return get8u(i);}
+inline int16_t sbuf_t::get16i(size_t i) const { return get16u(i);}
+inline int32_t sbuf_t::get32i(size_t i) const { return get32u(i);}
+inline int64_t sbuf_t::get64i(size_t i) const { return get64u(i);}
+inline int8_t sbuf_t::get8iBE(size_t i) const { return get8uBE(i);}
+inline int16_t sbuf_t::get16iBE(size_t i) const { return get16uBE(i);}
+inline int32_t sbuf_t::get32iBE(size_t i) const { return get32uBE(i);}
+inline int64_t sbuf_t::get64iBE(size_t i) const { return get64uBE(i);}
 
 inline int8_t sbuf_t::get8i(size_t i,sbuf_t::byte_order_t bo) const {
     return bo==BO_LITTLE_ENDIAN ? get8u(i) : get8uBE(i);
 }
 
 inline int16_t sbuf_t::get16i(size_t i,sbuf_t::byte_order_t bo) const {
-    return bo==BO_LITTLE_ENDIAN ? get16u(i) : get8uBE(i);
+    return bo==BO_LITTLE_ENDIAN ? get16u(i) : get16uBE(i);
 }
 
 inline int32_t sbuf_t::get32i(size_t i,sbuf_t::byte_order_t bo) const {
@@ -195,7 +139,7 @@ inline void sbuf_t::release()
     }
 #endif
     if(should_close && fd>0){
-	close(fd);
+        ::close(fd);
 	should_close = false;
 	fd=0;
     }
