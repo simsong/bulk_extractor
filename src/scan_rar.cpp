7b246fb1784c54954a89700c3b48fe16349c61d5
Michael Shick <mfshick@nps.edu>
2013-04-04 16:47:36 -0400

    added basic filename extraction to scan_rar +cleanup
    rar feature file will now feature the RAR'd filename just like scan_zip.
    additionally constants beginning with RAR_ no longer have that prefix since
    they aren't used in any other context

--- src/scan_rar.cpp
+++ src/scan_rar.cpp
@@ -10,39 +10,42 @@
 #include <iomanip>
 #include <cassert>
 
-#define RAR_FILE_MAGIC 0x74
-#define RAR_FILE_HEAD_MIN_LEN 32
+#define FILE_MAGIC 0x74
+#define FILE_HEAD_MIN_LEN 32
 
-#define RAR_OFFSET_HEAD_CRC 0
-#define RAR_OFFSET_HEAD_TYPE 2
-#define RAR_OFFSET_HEAD_FLAGS 3
-#define RAR_OFFSET_HEAD_SIZE 5
-#define RAR_OFFSET_PACK_SIZE 7
-#define RAR_OFFSET_UNP_SIZE 11
-#define RAR_OFFSET_HOST_OS 15
-#define RAR_OFFSET_FILE_CRC 16
-#define RAR_OFFSET_FTIME 20
-#define RAR_OFFSET_UNP_VER 24
-#define RAR_OFFSET_METHOD 25
-#define RAR_OFFSET_NAME_SIZE 26
-#define RAR_OFFSET_ATTR 28
-#define RAR_OFFSET_HIGH_PACK_SIZE 32
-#define RAR_OFFSET_HIGH_UNP_SIZE 36
+#define OFFSET_HEAD_CRC 0
+#define OFFSET_HEAD_TYPE 2
+#define OFFSET_HEAD_FLAGS 3
+#define OFFSET_HEAD_SIZE 5
+#define OFFSET_PACK_SIZE 7
+#define OFFSET_UNP_SIZE 11
+#define OFFSET_HOST_OS 15
+#define OFFSET_FILE_CRC 16
+#define OFFSET_FTIME 20
+#define OFFSET_UNP_VER 24
+#define OFFSET_METHOD 25
+#define OFFSET_NAME_SIZE 26
+#define OFFSET_ATTR 28
+#define OFFSET_HIGH_PACK_SIZE 32
+#define OFFSET_HIGH_UNP_SIZE 36
+#define OFFSET_FILE_NAME 32
 
-#define RAR_MANDATORY_FLAGS 0x8000
-#define RAR_UNUSED_FLAGS 0x6000
+#define MANDATORY_FLAGS 0x8000
+#define UNUSED_FLAGS 0x6000
 
-#define RAR_FLAG_CONT_PREV 0x0001
-#define RAR_FLAG_CONT_NEXT 0x0002
-#define RAR_FLAG_ENCRYPTED 0x0004
-#define RAR_FLAG_COMMENT 0x0008
-#define RAR_FLAG_SOLID 0x0010
-#define RAR_MASK_DICT 0x00E0
-#define RAR_FLAG_BIGFILE 0x0100
-#define RAR_FLAG_UNICODED 0x0200
-#define RAR_FLAG_SALTED 0x0400
-#define RAR_FLAG_OLD_VER 0x0800
-#define RAR_FLAG_EXTIME 0x1000
+#define FLAG_CONT_PREV 0x0001
+#define FLAG_CONT_NEXT 0x0002
+#define FLAG_ENCRYPTED 0x0004
+#define FLAG_COMMENT 0x0008
+#define FLAG_SOLID 0x0010
+#define MASK_DICT 0x00E0
+#define FLAG_BIGFILE 0x0100
+#define FLAG_UNICODED 0x0200
+#define FLAG_SALTED 0x0400
+#define FLAG_OLD_VER 0x0800
+#define FLAG_EXTIME 0x1000
+
+#define OPTIONAL_BIGFILE_LEN 8
 
 #define SUSPICIOUS_HEADER_LEN 1024
 
@@ -90,47 +93,69 @@ void scan_rar(const class scanner_params &sp,const recursion_control_block &rcb)
 	feature_recorder *rar_recorder = fs.get_name("rar");
 	rar_recorder->set_flag(feature_recorder::FLAG_XML); // because we are sending through XML
 	for(const unsigned char *cc=sbuf.buf;
-                cc < sbuf.buf+sbuf.pagesize && cc < sbuf.buf + sbuf.bufsize - RAR_FILE_HEAD_MIN_LEN;
+                cc < sbuf.buf+sbuf.pagesize && cc < sbuf.buf + sbuf.bufsize - FILE_HEAD_MIN_LEN;
                 cc++) {
             // Initial RAR file block anchor is 0x74 magic byte
-            if(cc[RAR_OFFSET_HEAD_TYPE] != RAR_FILE_MAGIC) {
+            if(cc[OFFSET_HEAD_TYPE] != FILE_MAGIC) {
                 continue;
             }
             // check for invalid flags
-            uint16_t flags = (uint16_t) int2(cc + RAR_OFFSET_HEAD_FLAGS);
-            if(!(flags & RAR_MANDATORY_FLAGS) || (flags & RAR_UNUSED_FLAGS)) {
+            uint16_t flags = (uint16_t) int2(cc + OFFSET_HEAD_FLAGS);
+            if(!(flags & MANDATORY_FLAGS) || (flags & UNUSED_FLAGS)) {
                 continue;
             }
             // ignore split files and encrypted files
-            if(flags & (RAR_FLAG_CONT_PREV | RAR_FLAG_CONT_NEXT | RAR_FLAG_ENCRYPTED)) {
+            if(flags & (FLAG_CONT_PREV | FLAG_CONT_NEXT | FLAG_ENCRYPTED)) {
                 continue;
             }
 
             // ignore impossible or improbable header lengths
-            uint16_t header_len = (uint16_t) int2(cc + RAR_OFFSET_HEAD_SIZE);
-            if(header_len < RAR_FILE_HEAD_MIN_LEN || header_len > SUSPICIOUS_HEADER_LEN) {
+            uint16_t header_len = (uint16_t) int2(cc + OFFSET_HEAD_SIZE);
+            if(header_len < FILE_HEAD_MIN_LEN || header_len > SUSPICIOUS_HEADER_LEN) {
                 continue;
+            }
+            // abort if header is longer than the remaining sbuf
+            if(header_len >= (sbuf.buf + sbuf.bufsize) - cc) {
+                break;
             }
 
             // ignore huge filename lengths
-            uint16_t filename_len = (uint16_t) int2(cc + RAR_OFFSET_NAME_SIZE);
+            uint16_t filename_len = (uint16_t) int2(cc + OFFSET_NAME_SIZE);
             if(filename_len > SUSPICIOUS_HEADER_LEN) {
                 continue;
             }
 
             // ignore strange file sizes
-            uint64_t packed_size = (uint64_t) int4(cc + RAR_OFFSET_PACK_SIZE);
-            uint64_t unpacked_size = (uint64_t) int4(cc + RAR_OFFSET_UNP_SIZE);
-            if(flags & RAR_FLAG_BIGFILE) {
-                packed_size += ((uint64_t) int4(cc + RAR_OFFSET_HIGH_PACK_SIZE)) << 32;
-                unpacked_size += ((uint64_t) int4(cc + RAR_OFFSET_HIGH_UNP_SIZE)) << 32;
+            uint64_t packed_size = (uint64_t) int4(cc + OFFSET_PACK_SIZE);
+            uint64_t unpacked_size = (uint64_t) int4(cc + OFFSET_UNP_SIZE);
+            if(flags & FLAG_BIGFILE) {
+                packed_size += ((uint64_t) int4(cc + OFFSET_HIGH_PACK_SIZE)) << 32;
+                unpacked_size += ((uint64_t) int4(cc + OFFSET_HIGH_UNP_SIZE)) << 32;
             }
             if(packed_size == 0 || unpacked_size == 0 || packed_size * 0.95 > unpacked_size) {
                 continue;
             }
 
+            const uint8_t *filename = cc + OFFSET_FILE_NAME;
+            if(flags & FLAG_BIGFILE) {
+                filename += OPTIONAL_BIGFILE_LEN;
+            }
+            if(flags & FLAG_UNICODED) {
+                //TODO deal with UTF-8 filenames
+            }
+            uint8_t filename_buf[SUSPICIOUS_HEADER_LEN + 1];
+            memset(filename_buf, 0x00, sizeof(filename_buf));
+            for(size_t ii = 0; ii < SUSPICIOUS_HEADER_LEN; ii++) {
+                if(filename[ii] == 0x00) {
+                    break;
+                }
+                filename_buf[ii] = filename[ii];
+            }
+
+            //cout << "Rar! <" << filename_buf << "> size: " << packed_size << "->" << unpacked_size << std::endl;
+
             ssize_t pos = cc-sbuf.buf; // position of the buffer
-            rar_recorder->write(pos0+pos,".","<rarinfo></rarinfo>");
+            rar_recorder->write(pos0+pos,string((const char *)filename_buf),"<rarinfo></rarinfo>");
 	}
     }
 }
