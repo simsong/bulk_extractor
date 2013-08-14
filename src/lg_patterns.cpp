010cce8b6591588df6d4d3be21d27bf862d30167
Joel Uckelman <uckelman@nomic.net>
2013-08-14 16:01:14 +0200

    Let liblightgrep handle rendering patterns as UTF-16LE.

--- src/lg_patterns.cpp
+++ src/lg_patterns.cpp
@@ -93,7 +93,7 @@ namespace accts {
 
   // FIXME: trailing context
   /* Possible BitLocker Recovery Key (UTF-16). */
-  const std::string BITLOCKER_UTF16("\\0((([0-9]\\0){6}-\\0){7}(([0-9]\\0){6}))[^0-9]");
+  const std::string BITLOCKER_UTF16("\\z00([0-9]{6}-){7}[0-9]{6}[^0-9]");
 
   // FIXME: trailing context
   /* Generalized international phone numbers */
@@ -156,11 +156,6 @@ namespace email {
   const std::string MONTH("(Jan|Feb|Mar|Apr|May|Jun|Jul|Aug|Sep|Oct|Nov|Dec)");
   const std::string ABBREV("(UT|GMT|EST|EDT|CST|CDT|MST|MDT|PST|PDT|[ZAMNY])");
 
-  const std::string U_TLD1("(A\\0C\\0|A\\0D\\0|A\\0E\\0|A\\0E\\0R\\0O\\0|A\\0F\\0|A\\0G\\0|A\\0I\\0|A\\0L\\0|A\\0M\\0|A\\0N\\0|A\\0O\\0|A\\0Q\\0|A\\0R\\0|A\\0R\\0P\\0A\\0|A\\0S\\0|A\\0S\\0I\\0A\\0|A\\0T\\0|A\\0U\\0|A\\0W\\0|A\\0X\\0|A\\0Z\\0|B\\0A\\0|B\\0B\\0|B\\0D\\0|B\\0E\\0|B\\0F\\0|B\\0G\\0|B\\0H\\0|B\\0I\\0|B\\0I\\0Z\\0|B\\0J\\0|B\\0L\\0|B\\0M\\0|B\\0N\\0|B\\0O\\0|B\\0R\\0|B\\0S\\0|B\\0T\\0|B\\0V\\0|B\\0W\\0|B\\0Y\\0|B\\0Z\\0|C\\0A\\0|C\\0A\\0T\\0|C\\0C\\0|C\\0D\\0|C\\0F\\0|C\\0G\\0|C\\0H\\0|C\\0I\\0|C\\0K\\0|C\\0L\\0|C\\0M\\0|C\\0N\\0|C\\0O\\0|C\\0O\\0M\\0|C\\0O\\0O\\0P\\0|C\\0R\\0|C\\0U\\0|C\\0V\\0|C\\0X\\0|C\\0Y\\0|C\\0Z\\0)");
-  const std::string U_TLD2("(D\\0E\\0|D\\0J\\0|D\\0K\\0|D\\0M\\0|D\\0O\\0|D\\0Z\\0|E\\0C\\0|E\\0D\\0U\\0|E\\0E\\0|E\\0G\\0|E\\0H\\0|E\\0R\\0|E\\0S\\0|E\\0T\\0|E\\0U\\0|F\\0I\\0|F\\0J\\0|F\\0K\\0|F\\0M\\0|F\\0O\\0|F\\0R\\0|G\\0A\\0|G\\0B\\0|G\\0D\\0|G\\0E\\0|G\\0F\\0|G\\0G\\0|G\\0H\\0|G\\0I\\0|G\\0L\\0|G\\0M\\0|G\\0N\\0|G\\0O\\0V\\0|G\\0P\\0|G\\0Q\\0|G\\0R\\0|G\\0S\\0|G\\0T\\0|G\\0U\\0|G\\0W\\0|G\\0Y\\0|H\\0K\\0|H\\0M\\0|H\\0N\\0|H\\0R\\0|H\\0T\\0|H\\0U\\0|I\\0D\\0|I\\0E\\0|I\\0L\\0|I\\0M\\0|I\\0N\\0|I\\0N\\0F\\0O\\0|I\\0N\\0T\\0|I\\0O\\0|I\\0Q\\0|I\\0R\\0|I\\0S\\0|I\\0T\\0|J\\0E\\0|J\\0M\\0|J\\0O\\0|J\\0O\\0B\\0S\\0|J\\0P\\0|K\\0E\\0|K\\0G\\0|K\\0H\\0|K\\0I\\0|K\\0M\\0|K\\0N\\0|K\\0P\\0|K\\0R\\0|K\\0W\\0|K\\0Y\\0|K\\0Z\\0)");
-  const std::string U_TLD3("(L\\0A\\0|L\\0B\\0|L\\0C\\0|L\\0I\\0|L\\0K\\0|L\\0R\\0|L\\0S\\0|L\\0T\\0|L\\0U\\0|L\\0V\\0|L\\0Y\\0|M\\0A\\0|M\\0C\\0|M\\0D\\0|M\\0E\\0|M\\0F\\0|M\\0G\\0|M\\0H\\0|M\\0I\\0L\\0|M\\0K\\0|M\\0L\\0|M\\0M\\0|M\\0N\\0|M\\0O\\0|M\\0O\\0B\\0I\\0|M\\0P\\0|M\\0Q\\0|M\\0R\\0|M\\0S\\0|M\\0T\\0|M\\0U\\0|M\\0U\\0S\\0E\\0U\\0M\\0|M\\0V\\0|M\\0W\\0|M\\0X\\0|M\\0Y\\0|M\\0Z\\0|N\\0A\\0|N\\0A\\0M\\0E\\0|N\\0C\\0|N\\0E\\0|N\\0E\\0T\\0|N\\0F\\0|N\\0G\\0|N\\0I\\0|N\\0L\\0|N\\0O\\0|N\\0P\\0|N\\0R\\0|N\\0U\\0|N\\0Z\\0|O\\0M\\0|O\\0R\\0G\\0|P\\0A\\0|P\\0E\\0|P\\0F\\0|P\\0G\\0|P\\0H\\0|P\\0K\\0|P\\0L\\0|P\\0M\\0|P\\0N\\0|P\\0R\\0|P\\0R\\0O\\0|P\\0S\\0|P\\0T\\0|P\\0W\\0|P\\0Y\\0)");
-  const std::string U_TLD4("(Q\\0A\\0|R\\0E\\0|R\\0O\\0|R\\0S\\0|R\\0U\\0|R\\0W\\0|S\\0A\\0|S\\0B\\0|S\\0C\\0|S\\0D\\0|S\\0E\\0|S\\0G\\0|S\\0H\\0|S\\0I\\0|S\\0J\\0|S\\0K\\0|S\\0L\\0|S\\0M\\0|S\\0N\\0|S\\0O\\0|S\\0R\\0|S\\0T\\0|S\\0U\\0|S\\0V\\0|S\\0Y\\0|S\\0Z\\0|T\\0C\\0|T\\0D\\0|T\\0E\\0L\\0|T\\0F\\0|T\\0G\\0|T\\0H\\0|T\\0J\\0|T\\0K\\0|T\\0L\\0|T\\0M\\0|T\\0N\\0|T\\0O\\0|T\\0P\\0|T\\0R\\0|T\\0R\\0A\\0V\\0E\\0L\\0|T\\0T\\0|T\\0V\\0|T\\0W\\0|T\\0Z\\0|U\\0A\\0|U\\0G\\0|U\\0K\\0|U\\0M\\0|U\\0S\\0|U\\0Y\\0|U\\0Z\\0|V\\0A\\0|V\\0C\\0|V\\0E\\0|V\\0G\\0|V\\0I\\0|V\\0N\\0|V\\0U\\0|W\\0F\\0|W\\0S\\0|Y\\0E\\0|Y\\0T\\0|Y\\0U\\0|Z\\0A\\0|Z\\0M\\0|Z\\0W\\0)");
-
   //
   // patterns
   //
@@ -199,14 +194,10 @@ namespace email {
   const std::string PROTO("(https?|afp|smb)://[a-zA-Z0-9_%/\\-+@:=&?#~.;]{1,384}");
  
   // FIXME: trailing context
-//  const std::string EMAIL_UTF16LE("[a-zA-Z0-9]\\0([a-zA-Z0-9._%\\-+]\\0){1,128}@\\0([a-zA-Z0-9._%\\-]\\0){1,128}\\.\\0(" + U_TLD1 + "|" + U_TLD2 + "|" + U_TLD3 + "|" + U_TLD4 + ")([^a-zA-Z]|([^][^\\0]))");
-
   const std::string EMAIL_UTF16LE("[a-zA-Z0-9][a-zA-Z0-9._%\\-+]{1,128}@[a-zA-Z0-9._%\\-]{1,128}\\." + TLD + "([^a-zA-Z]|[\\z00-\\zFF][^\\z00])");
 
   // FIXME: trailing context
-//  const std::string HTTP_UTF16LE("h\\0t\\0t\\0p\\0(s\\0)?:\\0([a-zA-Z0-9_%/\\-+@:=&?#~.;]\\0){1,128}([^a-zA-Z0-9_%/\\-+@:=&?#~.;]|([^][^\\0]))");
-
-  const std::string HTTP_UTF16LE("https?:([a-zA-Z0-9_%/\\-+@:=&?#~.;]){1,128}([^a-zA-Z0-9_%/\\-+@:=&?#~.;]|[\\z00-\\zFF][^\\z00]))");
+  const std::string HTTP_UTF16LE("https?:[a-zA-Z0-9_%/\\-+@:=&?#~.;]{1,128}([^a-zA-Z0-9_%/\\-+@:=&?#~.;]|[\\z00-\\zFF][^\\z00])");
 }
 
 namespace gps {
@@ -324,58 +315,66 @@ namespace httpheader {
   const std::string HEADERS_3("(Content-Length|Max-Forwards):[ \\t]?[0-9]{1,12}");
 }
 
+struct Pattern {
+  std::string pat, enc;
+};
+
+std::ostream& operator<<(std::ostream& o, Pattern p) {
+  return o << p.pat << "\t0\t0\t" << p.enc;
+}
+
 int main(int argc, char** argv) {
 
-  const char* all[] = {
-    accts::REGEX1.c_str(),
-    accts::REGEX2.c_str(),
-    accts::REGEX3.c_str(),
-    accts::REGEX4.c_str(),
-    accts::REGEX5.c_str(),
-    accts::REGEX6.c_str(),
-    accts::REGEX7.c_str(),
-    accts::REGEX8.c_str(),
-    accts::REGEX9.c_str(),
-    accts::REGEX10.c_str(),
-    accts::REGEX11.c_str(),
-    accts::REGEX12.c_str(),
-    accts::REGEX13.c_str(),
-    accts::REGEX14.c_str(),
-    accts::BITLOCKER_ASCII.c_str(),
-    accts::BITLOCKER_UTF16.c_str(),
-    accts::PDF_BOX.c_str(),
-    accts::PDF_RECT.c_str(),
-    base16::HEX1.c_str(),
-    base16::HEX2.c_str(),
-    email::DATE.c_str(),
-    email::MESSAGE_ID.c_str(),
-    email::SUBJECT.c_str(),
-    email::COOKIE.c_str(),
-    email::HOST.c_str(),
-    email::ADDR.c_str(),
-    email::NON_IP_1.c_str(),
-    email::NON_IP_2.c_str(),
-    email::IP.c_str(),
-    email::MAC.c_str(),
-    email::PROTO.c_str(),
-    email::EMAIL_UTF16LE.c_str(),
-    email::HTTP_UTF16LE.c_str(),
-    gps::TRKPT.c_str(),
-    gps::ELE.c_str(),
-    gps::TIME.c_str(),
-    gps::GPXTPX_SPEED.c_str(),
-    gps::GPXTPX_COURSE.c_str(),
-    httpheader::SERVER_OR_UA.c_str(),
-    httpheader::HOST.c_str(),
-    httpheader::HEADERS_1.c_str(),
-    httpheader::HEADERS_2.c_str(),
-    httpheader::VIA.c_str(),
-    httpheader::HEADERS_3.c_str()
+  const Pattern all[] = {
+    { accts::REGEX1,            "ASCII" },
+    { accts::REGEX2,            "ASCII" },
+    { accts::REGEX3,            "ASCII" },
+    { accts::REGEX4,            "ASCII" },
+    { accts::REGEX5,            "ASCII" },
+    { accts::REGEX6,            "ASCII" },
+    { accts::REGEX7,            "ASCII" },
+    { accts::REGEX8,            "ASCII" },
+    { accts::REGEX9,            "ASCII" },
+    { accts::REGEX10,           "ASCII" },
+    { accts::REGEX11,           "ASCII" },
+    { accts::REGEX12,           "ASCII" },
+    { accts::REGEX13,           "ASCII" },
+    { accts::REGEX14,           "ASCII" },
+    { accts::BITLOCKER_ASCII,   "ASCII" },
+    { accts::BITLOCKER_UTF16,   "UTF-16LE" },
+    { accts::PDF_BOX,           "ASCII" },
+    { accts::PDF_RECT,          "ASCII" },
+    { base16::HEX1,             "ASCII" },
+    { base16::HEX2,             "ASCII" },
+    { email::DATE,              "ASCII" },
+    { email::MESSAGE_ID,        "ASCII" },
+    { email::SUBJECT,           "ASCII" },
+    { email::COOKIE,            "ASCII" },
+    { email::HOST,              "ASCII" },
+    { email::ADDR,              "ASCII" },
+    { email::NON_IP_1,          "ASCII" },
+    { email::NON_IP_2,          "ASCII" },
+    { email::IP,                "ASCII" },
+    { email::MAC,               "ASCII" },
+    { email::PROTO,             "ASCII" },
+    { email::EMAIL_UTF16LE,     "UTF-16LE" },
+    { email::HTTP_UTF16LE,      "UTF-16LE" },
+    { gps::TRKPT,               "ASCII" },
+    { gps::ELE,                 "ASCII" },
+    { gps::TIME,                "ASCII" },
+    { gps::GPXTPX_SPEED,        "ASCII" },
+    { gps::GPXTPX_COURSE,       "ASCII" },
+    { httpheader::SERVER_OR_UA, "ASCII" },
+    { httpheader::HOST,         "ASCII" },
+    { httpheader::HEADERS_1,    "ASCII" },
+    { httpheader::HEADERS_2,    "ASCII" },
+    { httpheader::VIA,          "ASCII" },
+    { httpheader::HEADERS_3,    "ASCII" }
   };
 
   std::copy(
     all, all + sizeof(all)/sizeof(all[0]),
-    std::ostream_iterator<const char*>(std::cout, "\n")
+    std::ostream_iterator<Pattern>(std::cout, "\n")
   );
 
   return 0;
