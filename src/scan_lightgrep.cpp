b55b9f7282e1e1cf6b56cb8078e2a2b044ae4353
Joel Uckelman <uckelman@nomic.net>
2013-08-21 12:00:22 +0200

    Cleanup.

--- src/scan_lightgrep.cpp
+++ src/scan_lightgrep.cpp
@@ -9,7 +9,10 @@
 
 #include <lightgrep/api.h>
 
+#include <algorithm>
 #include <iostream>
+#include <iterator>
+#include <numeric>
 #include <sstream>
 
 using namespace std;
@@ -21,31 +24,33 @@ namespace { // local namespace hides these from other translation units
   static LG_HPROGRAM    lgProgram;
   static LG_HPATTERNMAP lgPatternMap;
 
-  vector< string > makePatterns() {
+  string beregex_pat(const beregex* ber) { return ber->pat; }
+
+  vector<string> makePatterns() {
     // get string from beregex list
-    vector< string > ret;
+    vector<string> ret;
 
-    const vector<beregex*>& bePatterns(find_list.patterns);
+    transform(
+      find_list.patterns.begin(), find_list.patterns.end(),
+      back_inserter(ret), beregex_pat
+    );
 
-    for (vector<beregex *>::const_iterator reItr(bePatterns.begin()); reItr != bePatterns.end(); ++reItr) {
-      ret.push_back((*reItr)->pat);
-    }
     return ret;
   }
 
-  vector< string > makeEncodings() {
-    vector< string > ret;
+  vector<string> makeEncodings() {
+    vector<string> ret;
     ret.push_back("UTF-8");
     ret.push_back("UTF-16LE");
     return ret;
   }
 
-  ssize_t sumPatternChars(const vector<string>& patterns) {
-    ssize_t count = 0;
-    for (vector<string>::const_iterator it(patterns.begin()); it != patterns.end(); ++it) {
-      count += it->size();
-    }
-    return count;
+  ssize_t slen(ssize_t count, const string& str) {
+    return count + str.size();
+  }
+
+  ssize_t totalChars(const vector<string>& strs) {
+    return accumulate(strs.begin(), strs.end(), 0, slen);
   }
 
   void cleanup(LG_HPATTERN pat, LG_HFSM fsm) {
@@ -54,7 +59,7 @@ namespace { // local namespace hides these from other translation units
   }
 
   void handleError(LG_HPATTERN pat, LG_HFSM fsm, LG_Error* lgErr) {
-    cerr << lgErr->Message << std::endl;
+    cerr << lgErr->Message << endl;
     cleanup(pat, fsm);
     lg_free_error(lgErr);
   }
@@ -64,20 +69,24 @@ namespace { // local namespace hides these from other translation units
     sp.info->author          = "Jon Stewart";
     sp.info->description     = "Advaned search for patterns";
     sp.info->scanner_version = "0.1";
-    sp.info->flags	     = scanner_info::SCANNER_FIND_SCANNER | scanner_info::SCANNER_FAST_FIND;
+    sp.info->flags	         = scanner_info::SCANNER_FIND_SCANNER |
+                               scanner_info::SCANNER_FAST_FIND;
     sp.info->feature_names.insert("lightgrep");
-    sp.info->histogram_defs.insert(histogram_def("lightgrep", "", "histogram", HistogramMaker::FLAG_LOWERCASE));
+    sp.info->histogram_defs.insert(histogram_def(
+      "lightgrep", "", "histogram", HistogramMaker::FLAG_LOWERCASE));
 
-    vector< string > patterns  = makePatterns(),
-                     encodings = makeEncodings();
+    vector<string> patterns(makePatterns()),
+                   encodings(makeEncodings());
 
     if (patterns.empty()) {
       return;
     }
 
-    unsigned    bytesPerCharEstimate = 4; // par for bytes per char in patterns, all encodings
+    // par for bytes per char in patterns, all encodings
+    const unsigned bytesPerCharEstimate = 4;
+
     LG_HPATTERN pattern = lg_create_pattern();
-    LG_HFSM     fsm     = lg_create_fsm(bytesPerCharEstimate * sumPatternChars(patterns));
+    LG_HFSM fsm = lg_create_fsm(bytesPerCharEstimate*totalChars(patterns));
 
     lgPatternMap = lg_create_pattern_map(patterns.size() * encodings.size());
 
@@ -88,9 +97,15 @@ namespace { // local namespace hides these from other translation units
     opts.CaseInsensitive = 0;
 
     // parse cross-product of patterns & encodings
-    for (vector<string>::const_iterator pat(patterns.begin()); pat != patterns.end(); ++pat) {
+    vector<string>::const_iterator pat(patterns.begin());
+    const vector<string>::const_iterator pend(patterns.end());
+
+    vector<string>::const_iterator enc;
+    const vector<string>::const_iterator eend(encodings.end());
+
+    for ( ; pat != pend; ++pat) {
       if (lg_parse_pattern(pattern, pat->c_str(), &opts, &lgErr)) {
-        for (std::vector<string>::const_iterator enc(encodings.begin()); enc != encodings.end(); ++enc) {
+        for (enc = encodings.begin(); enc != eend; ++enc) {
           if (0 > lg_add_pattern(fsm, lgPatternMap, pattern, enc->c_str(), &lgErr)) {
             handleError(pattern, fsm, lgErr);
             return;
@@ -102,6 +117,7 @@ namespace { // local namespace hides these from other translation units
         return;      
       }
     }
+
     // turn the parsed patterns into efficient bytecode representation
     LG_ProgramOptions progOpts;
     progOpts.Determinize = 1;
@@ -141,29 +157,31 @@ void scan_lightgrep(const class scanner_params &sp, const recursion_control_bloc
     if (!(lgPatternMap && lg_pattern_map_size(lgPatternMap) && lgProgram)) {
       return;
     }
-    // std::cout << "lightgrep scanning..." << std::endl;
+    // cout << "lightgrep scanning..." << endl;
     LG_ContextOptions ctxOpts;
     // these are debugging options that we'll remove
     ctxOpts.TraceBegin = 0xffffffffffffffff;
     ctxOpts.TraceEnd   = 0;
 
-    // a context maintains state related to searching a particular stream, built off of the program
-    // If BE maintained scanner state for different threads, then contexts could be reused, which would be a slight win.
+    // A context maintains state related to searching a particular stream,
+    // built from the program. If BE maintained scanner state for different
+    // threads, then contexts could be reused, which would be a slight win.
     LG_HCONTEXT ctx = lg_create_context(lgProgram, &ctxOpts);
 
-    const sbuf_t &sbuf   = sp.sbuf;
+    const sbuf_t &sbuf = sp.sbuf;
 
     HitData data(sp.fs.get_name("lightgrep"), &sbuf);
 
     // search the buffer all in one go
     lg_search(ctx, (const char*)sbuf.buf, (const char*)sbuf.buf + sbuf.bufsize, 0, &data, gotHit);
-    // if there are any hits at the end of the buffer that could extend with more data, lg_closeout_search will flush them out
+    // if there are any hits at the end of the buffer that could extend with
+    // more data, lg_closeout_search will flush them out
     lg_closeout_search(ctx, &data, gotHit);
 
     // cleanup
     lg_destroy_context(ctx);
 
-    // std::cout << data.Count << " hits on block " << sbuf.pos0 << std::endl;
+    // cout << data.Count << " hits on block " << sbuf.pos0 << endl;
   }
 }
 
