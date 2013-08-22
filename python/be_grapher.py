1eee79fcc4bbe0271acd0eee93f821f1385a9348
Michael Shick <mfshick@nps.edu>
2013-08-22 12:24:51 -0400

    be_grapher revision 1
    - uses line plot instead of bar chart
    - handles multiple plots on one figure
    - smoothed out y variable selection

--- python/be_grapher.py
+++ python/be_grapher.py
@@ -1,20 +1,21 @@
 #!/usr/bin/env python3
 
 import argparse
-import numpy
+import matplotlib.pyplot as plt
+import numpy as np
 import sys
 from bulk_extractor_reader import BulkReport
-from matplotlib import pyplot
 
 def main():
     args = proc_args()
 
+    group_var = args.group_var
     x_var = args.x_var
-    y_vars = list()
-    if args.show_clocktime:
-        y_vars += (AxisVar("clocktime"),)
+    y_vars = tuple()
     if args.show_memory:
         y_vars += (AxisVar("peak_memory"),)
+    else:
+        y_vars += (AxisVar("clocktime"),)
 
     if len(y_vars) < 1:
         print("all y values suppressed", file=sys.stderr)
@@ -22,52 +23,86 @@ def main():
 
     try:
         reports = tuple(sorted((BulkReport(x) for x in args.reports),
-                key=lambda x: x_var.of(x)))
+                key=lambda x: group_var.of(x)))
     except RuntimeError as e:
         print("failed to open reports: ", str(e), file=sys.stderr)
         return 1
 
+    # group related data into datasets
+    datasets = tuple()
+    dataset = tuple()
+    for report in reports:
+        if len(dataset) < 1:
+            dataset += (report,)
+            continue
+        if group_var.of(report) != group_var.of(dataset[0]):
+            datasets += (sorted(dataset, key=lambda x: x_var.of(x)),)
+            dataset = (report,)
+            continue
+        dataset += (report,)
+    datasets += (sorted(dataset, key=lambda x: x_var.of(x)),)
+    del(dataset)
+
     # check x uniqueness
-    if len(set([x_var.of(report) for report in reports])) != len(reports):
-        print("x value ({}) must be different for each report"
-                .format(x_var.name), file=sys.stderr)
-        return 1
+    for dataset in datasets:
+        if len(set([x_var.of(report) for report in dataset])) != len(dataset):
+            print("x value ({}) must be unique per grouping ({})"
+                    .format(x_var.name, group_var.name), file=sys.stderr)
+            return 1
 
-    make_plot(reports, x_var, y_vars, args.output_filename)
+    make_plot(datasets, x_var, y_vars, group_var, args.output_filename)
 
-def make_plot(reports, x_var, y_vars, filename):
+def make_plot(datasets, x_var, y_vars, group_var, filename):
     # TODO both time and mem
     y_var = y_vars[0]
 
-    fig = pyplot.figure()
-    ax = fig.add_axes([0.25, 0.1, 0.7, 0.8])
+    # use the longest dataset as the prototypical dataset
+    exemplar = max(datasets, key=len)
+    x_values = tuple((str(x_var.of(report)) for report in exemplar))
+    # list of lists of y values
+    y_valueses = tuple()
 
-    numbins = len(reports)
-    barHeight = 0.6
+    minima = tuple()
+    maxima = tuple()
+    plot_labels = tuple()
 
-    pyplot.title("bulk_extractor Benchmark")
+    for dataset in datasets:
+        y_valueses += (tuple((y_var.of(report) for report in dataset)),)
+        minima += (min(y_valueses[-1]),)
+        maxima += (max(y_valueses[-1]),)
+        plot_labels += (group_var.name + " " + str(group_var.of(dataset[0])),)
 
-    for label in ax.yaxis.get_ticklabels():
-        label.set_fontsize(6)
-    for label in ax.xaxis.get_ticklabels():
-        label.set_fontsize(7)
+    y_min = min(minima)
+    y_max = max(maxima)
+    y_range = y_max - y_min
 
-    ax.xaxis.get_label().set_fontsize(8.5)
-    ax.yaxis.get_label().set_fontsize(8.5)
-    
-    pyplot.xlabel(x_var.label)
-    pyplot.ylabel(y_var.label)
+    fig = plt.figure()
+    ax = fig.add_axes((0.1, 0.2, 0.8, 0.7))
 
-    x_values = tuple((x_var.of(report) for report in reports))
-    y_values = tuple((y_var.of(report) for report in reports))
+    # create open-looking plot by removing top and right spines and ticks
+    ax.spines['right'].set_color('none')
+    ax.spines['top'].set_color('none')
+    ax.xaxis.set_ticks_position('bottom')
+    ax.yaxis.set_ticks_position('left')
 
-    pyplot.xticks(numpy.arange(numbins)+1.5*barHeight/2, x_values)
-    rects = pyplot.bar(left=numpy.arange(numbins)+1.5*barHeight/2,
-                     height=y_values,
-                     width=barHeight,
-                     align='center')
+    # pad the plot a bit for aesthetics
+    ax.set_xlim([-0.5, len(exemplar) - 0.5])
+    ax.set_ylim([y_min - y_range * 0.1,
+        y_max + y_range * (0.15 * len(plot_labels))])
 
-    pyplot.savefig(filename, format='pdf')
+    ax.set_xticks(np.arange(len(exemplar)))
+    ax.set_xticklabels(x_values)
+
+    for y_values, plot_label in zip(y_valueses, plot_labels):
+        plt.plot(y_values, label=plot_label)
+
+    plt.title("bulk_extractor Benchmark")
+    plt.xlabel(x_var.label)
+    plt.ylabel(y_var.label)
+    if group_var.name != "none":
+        plt.legend()
+
+    plt.savefig(filename, format='pdf')
 
 class AxisVar:
     def __init__(self, var_name):
@@ -90,6 +125,9 @@ class AxisVar:
         elif var_name == "peak_memory":
             self.of = lambda x: x.peak_memory() / 1024
             self.label = "Peak Memory Usage (MiB)"
+        elif var_name == "none":
+            self.of = lambda x: 0
+            self.label = "Nothing"
         else:
             raise ValueError("unknown variable name " + str(var_name))
 
@@ -101,14 +139,15 @@ def proc_args():
     arg_parser.add_argument("-x", dest="x_var", type=AxisVar, default="version",
             help=("x axis variable (version, threads, page_size, or " +
                 "margin_size)"))
+    arg_parser.add_argument("-g", "--grouping", dest="group_var", type=AxisVar,
+            default=AxisVar("none"), help="variable to group results by")
     arg_parser.add_argument("-o", dest="output_filename",
             default="be_graph.pdf", help="(default \"be_graph.pdf\")")
-    arg_parser.add_argument("--hide-clocktime", dest="show_clocktime",
-            action="store_false", help="do not show clocktime on the y axis")
-    arg_parser.add_argument("--hide-memory", dest="show_memory",
-            action="store_false", help="do not show peak memory on the y axis")
+    arg_parser.add_argument("--memory", dest="show_memory",
+            action="store_true", help=("plot bulk_extractor runs agains " +
+                "their peak memory usage instead of clock time"))
     args = arg_parser.parse_args()
     return args
 
 if __name__ == "__main__":
-    main()
+    sys.exit(main())
