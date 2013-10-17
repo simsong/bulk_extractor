0a24c28b39fcf0a3877d2fe6a3a78b0fa5a1c18c
Joel Uckelman <uckelman@nomic.net>
2013-10-17 12:08:14 +0200

    Added $OPTS to make it easy to specify options (e.g. "-j 1") for test runs.

--- lgcomp.sh
+++ lgcomp.sh
@@ -8,10 +8,12 @@ mkdir -p $TDIR
 
 export LD_LIBRARY_PATH=/usr/local/lib
 
-src/bulk_extractor -x accts_lg -x base16_lg -x email_lg -x gps_lg -x lightgrep -o $ODIR $1
+#OPTS="-j 1"
+
+src/bulk_extractor -x accts_lg -x base16_lg -x email_lg -x gps_lg -x lightgrep -o $ODIR $OPTS $1
 mv $ODIR $TDIR/${GITHASH}
 
-src/bulk_extractor -x accts -x base16 -x email -x gps -o $ODIR $1
+src/bulk_extractor -x accts -x base16 -x email -x gps -o $ODIR $OPTS $1
 mv $ODIR $TDIR/${GITHASH}_lg
 
 CMPDIR=$TDIR/${GITHASH}_cmp
