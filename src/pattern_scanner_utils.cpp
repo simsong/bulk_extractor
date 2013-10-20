23d13597681765159ef9cf014711ad811ca42ecc
Simson Garfinkel <simsong@acm.org>
2013-10-19 22:06:09 -0400

    fixed warning and overflow

--- src/pattern_scanner_utils.cpp
+++ src/pattern_scanner_utils.cpp
@@ -4,7 +4,7 @@ string low_utf16le_to_ascii(const uint8_t* buf, size_t len) {
   // When UTF-16LE is restricted to code points <= U+7F,
   // removing every odd byte converts it to ASCII encoding.
   const size_t olen = len/2;
-  string out('\0', olen);
+  string out(olen,'\0');
 
   for (size_t i = 0; i < olen; ++i) {
     out[i] = buf[2*i];
