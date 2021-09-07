554246b7b3b832574943c21c32b28ea06ece24e3
Simson L. Garfinkel <simsong@acm.org>
2021-09-07 14:50:13 -0400

    overflow bug in scan_aes

--- src/scan_aes.cpp
+++ src/scan_aes.cpp
@@ -406,19 +406,22 @@ void scan_aes(struct scanner_params &sp)
 	for (size_t pos = 0 ; pos < sp.sbuf->bufsize && pos < sp.sbuf->pagesize; pos++){
             /* TODO: Remove direct memory access with mediated access */
             const uint8_t *p2 = sp.sbuf->get_buf() + pos;
-	    if (scan_aes_128 && sp.sbuf->distinct_characters( pos, AES128_KEY_SIZE) > AES128_KEY_SIZE/4){
+	    if (scan_aes_128 && (sp.sbuf->bufsize >= AES128_KEY_SCHEDULE_SIZE) &&
+                (sp.sbuf->distinct_characters( pos, AES128_KEY_SIZE) > AES128_KEY_SIZE/4)){
 		if (valid_aes128_schedule(p2)) {
                     std::string key = key_to_string(p2, AES128_KEY_SIZE);
 		    aes_recorder.write(sp.sbuf->pos0+pos,key,std::string("AES128"));
 		}
             }
-	    if (scan_aes_192 && sp.sbuf->distinct_characters( pos, AES192_KEY_SIZE) > AES192_KEY_SIZE/4){
+	    if (scan_aes_192 && (sp.sbuf->bufsize>= AES192_KEY_SCHEDULE_SIZE) &&
+                (sp.sbuf->distinct_characters( pos, AES192_KEY_SIZE) > AES192_KEY_SIZE/4)){
 		if (valid_aes192_schedule(p2)) {
                     std::string key = key_to_string(p2, AES192_KEY_SIZE);
 		    aes_recorder.write(sp.sbuf->pos0+pos,key,std::string("AES192"));
 		}
             }
-	    if (scan_aes_256 && sp.sbuf->distinct_characters( pos, AES256_KEY_SIZE) > AES256_KEY_SIZE/4){
+	    if (scan_aes_256 && (sp.sbuf->bufsize>=AES256_KEY_SCHEDULE_SIZE) &&
+                (sp.sbuf->distinct_characters( pos, AES256_KEY_SIZE) > AES256_KEY_SIZE/4)){
 		if (valid_aes256_schedule(p2)) {
                     std::string key = key_to_string(p2, AES256_KEY_SIZE);
 		    aes_recorder.write(sp.sbuf->pos0+pos,key,std::string("AES256"));
