8e9ef879b37c75eed561c01c21df6a68a7063f86
Simson Garfinkel <simsong@acm.org>
2021-10-30 17:11:40 +0000

    update

--- etc/CONFIGURE_UBUNTU20LTS.bash
+++ etc/CONFIGURE_UBUNTU20LTS.bash
@@ -4,10 +4,12 @@ CONFIGURE="./configure -q --enable-silent-rules"
 LIBEWF_DIST=https://github.com/libyal/libewf-legacy/releases/download/20140812/libewf-20140812.tar.gz
 AUTOCONF_DIST=https://ftpmirror.gnu.org/autoconf/autoconf-2.71.tar.gz
 AUTOMAKE_DIST=https://ftpmirror.gnu.org/automake/automake-1.16.3.tar.gz
-MKPGS="autoconf automake libexpat1-dev libssl-dev libtool libxml2-utils pkg-config"
+MKPGS="autoconf automake build-essential libexpat1-dev libssl-dev libtool libxml2-utils make pkg-config"
 WGET="wget -nv --no-check-certificate"
 CONFIGURE="./configure -q --enable-silent-rules"
 MAKE="make -j4"
+trap "exit 1" TERM
+export TOP_PID=$$
 cat <<EOF
 *******************************************************************
         Configuring Ubuntu $RELEASE.04 LTS to compile bulk_extractor.
@@ -24,15 +26,19 @@ press any key to continue...
 EOF
 read IGNORE
 
+function fail() {
+	echo FAIL: $@
+	kill -s TERM $TOP_PID
+}
+
 # cd to the directory where the script is
 # http://stackoverflow.com/questions/59895/can-a-bash-script-tell-what-directory-its-stored-in
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-if [ "$PWD" != "$DIR" ]; then
-    changed_dir="true"
-else
-    changed_dir="false"
-fi
+DIR="$(dirname "${BASH_SOURCE[0]}")"
 cd $DIR
+mkdir -p tmp
+/bin/rm -rf tmp
+mkdir -p tmp
+cd tmp
 
 if [ ! -r /etc/os-release ]; then
     echo This requires an /etc/os-release file.
@@ -45,6 +51,7 @@ if [ x$ID != xubuntu ]; then
     echo This really requires ubuntu. You have $ID
     exit 1
 fi
+
 MAJOR_VERSION=`echo $VERSION_ID|sed s/[.].*//`
 if [ $MAJOR_VERSION -lt $RELEASE ]; then
     echo This requires at least Ubuntu $RELEASE Linux.
@@ -53,14 +60,17 @@ fi
 
 echo Will now try to install
 
-sudo apt update -y
-sudo apt install -y $MKPGS
+sudo apt update -y || fail could not apt update
+sudo apt install -y $MKPGS || fail could not apt install $MKPGS
+
+fail here
 
 echo manually installing a modern libewf
 $WGET $LIBEWF_DIST || (echo could not download $LIBEWF_DIST; exit 1)
 tar xfz libewf*gz   && (cd libewf*/   && $CONFIGURE && $MAKE >/dev/null && sudo make install)
 ls -l /etc/ld.so.conf.d/
 sudo ldconfig
+ewfinfo -h >/dev/null || (echo could not install libewf; exit 1)
 
 echo updating autoconf
 $WGET $AUTOCONF_DIST || (echo could not download $AUTOCONF_DIST; exit 1)
@@ -72,6 +82,6 @@ $WGET $AUTOMAKE_DIST || (echo could not download $AUTOMAKE_DIST; exit 1)
 tar xfz automake*gz && (cd automake*/ && $CONFIGURE && $MAKE >/dev/null && sudo make install)
 automake --version
 
-cd ..
-(bash bootstrap.sh  && ./configure -q --enable-silent-rules && make && make check) || (echo make check failed;exit 1)
+cd $DIR/..
+(bash bootstrap.sh  && $CONFIGURE && make && make check) || (echo make check failed;exit 1)
 
