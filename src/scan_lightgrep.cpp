ea2dbf40e32564fee8346dcce5405d6fcd247f05
Jon Stewart <jonathan.l.stewart@gmail.com>
2022-06-06 15:58:49 -0400

    Stub out a new Lightgrep controller, with setup() and teardown() called in Init2 and Shutdown phases, respectively

--- src/scan_lightgrep.cpp
+++ src/scan_lightgrep.cpp
@@ -14,54 +14,20 @@
 
 #include <lightgrep/api.h>
 
+namespace {
+    class LightgrepController {
+    public:
+        LightgrepController() {}
 
-// namespace { // local namespace hides these from other translation units
-
-//   class FindScanner: public PatternScanner {
-//   public:
-//     FindScanner(): PatternScanner("lightgrep"), LgRec(0) {}
-//     virtual ~FindScanner() {}
-
-//     virtual FindScanner* clone() const {
-//       return new FindScanner(*this);
-//     };
-
-//     virtual void startup(const scanner_params& sp) {
-//         sp.info.set_name("scan_lightgrep");
-//         sp.info->author          = "Jon Stewart";
-//         sp.info->description     = "Advanced search for patterns";
-//         sp.info->scanner_version = "0.2";
-//         sp.info->flags           = scanner_info::SCANNER_FIND_SCANNER | scanner_info::SCANNER_FAST_FIND;
-//         sp.info->feature_names.insert(name());
-//         sp.info->histogram_defs.insert(histogram_def( name(), "", "histogram", HistogramMaker::FLAG_LOWERCASE));
-//     }
-
-//     virtual void init(const scanner_params& sp) {
-//     }
-
-//     virtual void initScan(const scanner_params& sp) {
-//       LgRec = &sp.named_feature_recorder(name());
-//     }
-
-//     feature_recorder* LgRec;
-
-//     void processHit(const LG_SearchHit& hit, const scanner_params& sp) {
-//       LgRec->write_buf(sp.sbuf, hit.Start, hit.End - hit.Start);
-//     }
-
-//   private:
-//     FindScanner(const FindScanner& x): PatternScanner(x), LgRec(x.LgRec) {}
-
-//     FindScanner& operator=(const FindScanner&);
-//   };
-
-//   FindScanner Scanner;
-
-//   CallbackFnType ProcessHit;
-// }
+        void setup() {}
+        void teardown() {}
+    };
+}
 
 extern "C"
 void scan_lightgrep(struct scanner_params &sp) {
+    static LightgrepController LG;
+
     switch (sp.phase) {
     case scanner_params::PHASE_INIT:
         std::cerr << "scan_lightgrep Init called\n";
@@ -80,6 +46,7 @@ void scan_lightgrep(struct scanner_params &sp) {
     case scanner_params::PHASE_INIT2:
         {
             std::cerr << "scan_lightgrep Init2 called\n";
+            LG.setup();
           // Scanner.init(sp);
           // LightgrepController& lg(LightgrepController::Get());
           // lg.addUserPatterns(Scanner, &ProcessHit, sp.ss->sc); // note: FindOpts now passed in ScannerConfig
@@ -93,6 +60,7 @@ void scan_lightgrep(struct scanner_params &sp) {
     case scanner_params::PHASE_SHUTDOWN:
         std::cerr << "scan_lightgrep Shutdown called\n";
         // Scanner.shutdown(sp);
+        LG.teardown();
         break;
     default:
         break;
