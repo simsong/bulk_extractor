964659649ad7774cc3a158cac0df0d8805487e0b
Simson Garfinkel <simsong@acm.org>
2024-04-26 20:21:16 -0400

    added stuff to MACOS to add support for /opt/local and /opt/homebrew for building.

--- etc/CONFIGURE_MACOS.bash
+++ etc/CONFIGURE_MACOS.bash
@@ -30,13 +30,13 @@ PKGS+="wget libtool autoconf automake libtool libxml2 libewf json-c re2 abseil p
 $WHICH install $PKGS || (echo installation install failed; exit 1)
 
 echo ================
-echo Updating .profile
-echo 'PATH="/opt/homebrew/opt/libtool/libexec/gnubin:$PATH"' >> $HOME/.profile
-echo 'export PATH="/opt/homebrew/opt/libxml2/bin:$PATH"' >> $HOME/.profile
-echo 'export LDFLAGS="-L/opt/homebrew/lib -L/opt/homebrew/opt/libxml2/lib $LDFLAGS" ' >> $HOME/.profile
-echo 'export CPPFLAGS="-I/opt/homebrew/include -I/opt/homebrew/opt/libxml2/include $CPPFLAGS" ' >> $HOME/.profile
-echo 'export PKG_CONFIG_PATH="/opt/homebrew/opt/libxml2/lib/pkgconfig:$PKG_CONFIG_PATH"' >> $HOME/.profile
+echo Updating .bashrc
+echo 'PATH="/opt/homebrew/opt/libtool/libexec/gnubin:$PATH"' >> $HOME/.bashrc
+echo 'export PATH="/opt/homebrew/opt/libxml2/bin:$PATH"' >> $HOME/.bashrc
+echo 'export LDFLAGS="-L/opt/homebrew/lib -L/opt/homebrew/opt/libxml2/lib $LDFLAGS" ' >> $HOME/.bashrc
+echo 'export CPPFLAGS="-I/opt/homebrew/include -I/opt/homebrew/opt/libxml2/include $CPPFLAGS" ' >> $HOME/.bashrc
+echo 'export PKG_CONFIG_PATH="/opt/homebrew/opt/libxml2/lib/pkgconfig:$PKG_CONFIG_PATH"' >> $HOME/.bashrc
 echo ================
-source $HOME/.profile
+source $HOME/.bashrc
 printenv
 exit 0
