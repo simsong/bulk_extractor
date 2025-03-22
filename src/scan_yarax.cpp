af88d0e71ed945f98e4a8594a6a8dc6af9c2c490
Jon Stewart <jonathan.l.stewart@gmail.com>
2025-03-22 10:28:31 -0400

    basic scanning of sbufs with yara_x implemented. This uses a hard-coded rule that is unlikely to generate matches and it also does not use feature-recorders to record any matches. But it does get called and pass tests with the address_sanitizer. I will need to think of some ways to unit test this.

--- src/scan_yarax.cpp
+++ src/scan_yarax.cpp
@@ -10,7 +10,38 @@
 #include "be20_api/scanner_params.h"
 
 #ifdef HAVE_YARAX
-extern "C" void scan_yarax(scanner_params &sp);
+extern "C" {
+#include <yara_x.h>
+
+void scan_yarax(scanner_params &sp);
+}
+
+auto YARA_RULE = R"(
+rule test_rule {
+  strings:
+    $a = "asdfoiqweofiuxkbjvlkjdlksdjflaisjflkvlkfvnbzkdfjasoei;lzkxcvnkaslfkjqlkj"
+  condition:
+    $a
+}
+)";
+
+using rules_ptr = std::unique_ptr<YRX_RULES, decltype(&yrx_rules_destroy)>;
+
+rules_ptr& getYaraRules() {
+  static rules_ptr rules(nullptr, yrx_rules_destroy);
+  return rules;
+}
+
+void yara_callback(const YRX_RULE* rule, void*) {
+  const uint8_t* ruleID = nullptr;
+  size_t   idLen = 0;
+  if (yrx_rule_identifier(rule, &ruleID, &idLen) != SUCCESS) {
+    std::cerr << "Failed to get rule ID in yara_callback" << std::endl;
+    return;
+  }
+  std::string_view ruleIDView(reinterpret_cast<const char*>(ruleID), idLen);
+  std::cout << "Matched rule: " << ruleIDView << std::endl;
+}
 
 void scan_yarax(scanner_params &sp) {
   sp.check_version();
@@ -20,6 +51,51 @@ void scan_yarax(scanner_params &sp) {
     sp.info->description     = "Scans for yara-x rule matches in raw pages";
     sp.info->scanner_version = "1.0";
     return;
+  }
+  else if (sp.phase == scanner_params::PHASE_INIT2) {
+    YRX_RULES* rules = nullptr;
+    YRX_RESULT result = yrx_compile(YARA_RULE, &rules);
+    if (result != SUCCESS) {
+      std::cerr << "Failed to compile yara-x rule: " << yrx_last_error() << std::endl;
+      return;
+    }
+    else {
+      getYaraRules().reset(rules);
+    }
+  }
+  else if (sp.phase == scanner_params::PHASE_SCAN) {
+    rules_ptr& rules = getYaraRules();
+    if (!rules) {
+      return;
+    }
+    YRX_SCANNER* scannerRawPtr = nullptr;
+    YRX_RESULT result = yrx_scanner_create(rules.get(), &scannerRawPtr);
+    if (result != SUCCESS) {
+      std::cerr << "Failed to create yara-x scanner: " << yrx_last_error() << std::endl;
+      return;
+    }
+    std::unique_ptr<YRX_SCANNER, decltype(&yrx_scanner_destroy)> scanner(scannerRawPtr, yrx_scanner_destroy);
+
+    result = yrx_scanner_on_matching_rule(scanner.get(), yara_callback, nullptr);
+    if (result != SUCCESS) {
+      std::cerr << "Failed to set yara-x callback: " << yrx_last_error() << std::endl;
+      return;
+    }
+
+    // Iterate the sbuf 4KB at a time and scan these pages individually
+    const sbuf_t *sbuf = sp.sbuf;
+    const size_t blockSize = 4096;
+    const uint8_t* curBlock = sbuf->get_buf();
+    const uint8_t* end = curBlock + sbuf->pagesize;
+    while (curBlock < end) {
+      const size_t len = std::min(blockSize, static_cast<size_t>(end - curBlock));
+      result = yrx_scanner_scan(scanner.get(), curBlock, len);
+      if (result != SUCCESS) {
+        std::cerr << "Failed to scan yara-x: " << yrx_last_error() << std::endl;
+        return;
+      }
+      curBlock += len;
+    }
   }
 }
 #endif
